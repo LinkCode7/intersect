1abad4dc01fee502f919761bf8f7877460b3ce29
zhangshangliang <zhangshangliang@300624.cn>
2022-09-07 14:09:29 +0800

    添加忽略文件，修改SQLite格式，不影响逻辑

--- SQLite/SQLiteUtility.h
+++ SQLite/SQLiteUtility.h
@@ -1,130 +1,132 @@
-#pragma once
-#include "../SindyGlobal.h"
+#ifndef SQLITE_UTILITY_U
+#define SQLITE_UTILITY_U
+
 #include "sqlite3.h"
+
+#include <map>
+#include <string>
 
 struct sqlite3;
 struct sqlite3_stmt;
 
+#define SINDY_API
+// using sindy_int64 = __int64;
+using sindy_int64 = int64_t;
 
+namespace Sindy {
 
-namespace Sindy
+using PString = std::string;
+
+class SINDY_API SQLite
 {
-	// sqlite3采用utf16编码，即wstring
-	class SINDY_API SQLite
-	{
-	public:
-		explicit SQLite(const PString& strDbPath = ":memory:");
-		explicit SQLite(const SQLite& opened);
-		~SQLite();
+public:
+    explicit SQLite(const PString &strDbPath = ":memory:");
+    explicit SQLite(const SQLite &opened);
+    ~SQLite();
 
-		int open(const PString& strSqliteDbPath);
-		int close();
+    int open(const PString &strSqliteDbPath);
+    int close();
 
-	public:
+public:
+    // 在同一时间使用多个数据库中的一个
+    // ATTACH DATABASE语句使用后，所有的语句将在附加的数据库下执行
+    int attach(const PString &strDBPath, const PString &strDBName);
+    int detach(const PString &strDBName);
 
-		// 在同一时间使用多个数据库中的一个
-		// ATTACH DATABASE语句使用后，所有的语句将在附加的数据库下执行
-		int attach(const PString& strDBPath, const PString& strDBName);
-		int detach(const PString& strDBName);
+    // 将SQL语句编译为sqlite3_stmt，该结构体包含了将要执行的SQL语句信息
+    int prepare(const PString &strSql);
 
-		// 将SQL语句编译为sqlite3_stmt，该结构体包含了将要执行的SQL语句信息
-		int prepare(const PString& strSql);
+    // 执行语句，SQLITE_DONE和SQLITE_ROW都是表示执行成功，SQLITE_DONE表示没有查询结果
+    // 每次step只返回一行数据，SQLITE_OK表示执行结束并有查询结果
+    int step();
 
-		// 执行语句，SQLITE_DONE和SQLITE_ROW都是表示执行成功，SQLITE_DONE表示没有查询结果
-		// 每次step只返回一行数据，SQLITE_OK表示执行结束并有查询结果
-		int step();
+    // 释放语句对象，在SQL语句使用完之后，内存是prepare申请的
+    int finalize();
 
-		// 释放语句对象，在SQL语句使用完之后，内存是prepare申请的
-		int finalize();
+    // 重置语句对象到它的初始状态，准备被重新执行（如果SQL语句对象要重复使用）
+    int resetSyntax();
 
-		// 重置语句对象到它的初始状态，准备被重新执行（如果SQL语句对象要重复使用）
-		int resetSyntax();
+    // 使用场景：不返回结果集的单一语句
+    int execute(const PString &strSql);
 
-		// 使用场景：不返回结果集的单一语句
-		int execute(const PString& strSql);
+public:
+    // 结果集
+    PString getValueText(int iCol);
+    bool    getValueText(const PString &strColName, PString &strColValue);
 
-	public:
+    bool   getValueDouble(const PString &strColName, double &dblColValue);
+    double getValueDouble(int iCol);
 
-		// 结果集
-		PString getValueText(int iCol);
-		bool getValueText(const PString& strColName, PString& strColValue);
+    bool getValueInt(const PString &strColName, int &iColValue);
+    int  getValueInt(int iCol);
 
-		bool getValueDouble(const PString& strColName, double& dblColValue);
-		double getValueDouble(int iCol);
+    // 大字段
+    long        largeFieldSize(int iCol);
+    const void *getValueLargeField(int iCol);
+    bool        getValueLargeField(const PString &strColName, const void *&pBuffer);
+    bool        getValueLargeField(const PString &strColName, const void *&pBuffer, long &lBufferSize);
 
-		bool getValueInt(const PString& strColName, int& iColValue);
-		int getValueInt(int iCol);
+    // 获取记录的字段个数,只有语句句柄具有活动游标才可以工作
+    int getFieldCount();
+    // 获取列的值类型
+    int getFieldType(const PString &strColName);
 
-		// 大字段
-		long largeFieldSize(int iCol);
-		const void* getValueLargeField(int iCol);
-		bool getValueLargeField(const PString& strColName, const void*& pBuffer);
-		bool getValueLargeField(const PString& strColName, const void*& pBuffer, long& lBufferSize);
+    void    initColName2ColIndex();
+    PString columnName(int iCol);
 
-		// 获取记录的字段个数,只有语句句柄具有活动游标才可以工作
-		int getFieldCount();
-		// 获取列的值类型
-		int getFieldType(const PString& strColName);
+    // 绑定数据
+    int bindText(const PString &strColName, const PString &strValue);
+    int bindText(int iIndex, const PString &strValue);
 
-		void initColName2ColIndex();
-		PString columnName(int iCol);
+    int bindInt(int iIndex, int iValue);
+    int bindInt(const PString &strColName, int iValue);
+    int bindInt64(int iIndex, sindy_int64 llValue);
+    int bindInt64(const PString &strColName, sindy_int64 llValue);
 
+    int bindDouble(int iIndex, double dblValue);
+    int bindDouble(const PString &strColName, double dblValue);
 
+    int bindLargeField(int iIndex, const void *pBuffer, int iByte);
+    int bindLargeField(const PString &strColName, const void *pBuffer, int iByte);
 
-		// 绑定数据
-		int bindText(const PString& strColName, const PString& strValue);
-		int bindText(int iIndex, const PString& strValue);
+    int bindParameterIndex(const PString &strColName);
 
-		int bindInt(int iIndex, int iValue);
-		int bindInt(const PString& strColName, int iValue);
-		int bindInt64(int iIndex, __int64 llValue);
-		int bindInt64(const PString& strColName, __int64 llValue);
+    // 清空绑定值
+    int clearBindings();
 
-		int bindDouble(int iIndex, double dblValue);
-		int bindDouble(const PString& strColName, double dblValue);
+public:
+    // 删除
+    int dropTable(const PString &strTableName, const PString &strDbName = "main");
+    int dropView(const PString &strTableName, const PString &strDbName = "main");
+    int clearTable(const PString &strTableName, const PString &strDbName = "main");
 
-		int bindLargeField(int iIndex, const void* pBuffer, int iByte);
-		int bindLargeField(const PString& strColName, const void* pBuffer, int iByte);
+    // 是否存在
+    bool isTableExist(const PString &strTableName, const PString &strDbName = "main");
+    bool isViewExist(const PString &strTableName, const PString &strDbName = "main");
+    bool isFieldExist(const PString &strTableName, const PString &strFieldName, const PString &strDbName = "main");
 
-		int bindParameterIndex(const PString& strColName);
+public:
+    // 开启事务，在需要多次执行SQL语句时，先开启事务可以提升很大效率
+    // SQLite缺省为每个操作启动一个事务
+    void beginTransaction();
 
-		// 清空绑定值
-		int clearBindings();
+    // 回滚
+    void rollBack();
 
-	public:
+    // 提交
+    void commit();
 
-		// 删除
-		int dropTable(const PString& strTableName, const PString& strDbName = "main");
-		int dropView(const PString& strTableName, const PString& strDbName = "main");
-		int clearTable(const PString& strTableName, const PString& strDbName = "main");
+    // 获取最近一次错误信息
+    PString errorMessage();
 
-		// 是否存在
-		bool isTableExist(const PString& strTableName, const PString& strDbName = "main");
-		bool isViewExist(const PString& strTableName, const PString& strDbName = "main");
-		bool isFieldExist(const PString& strTableName, const PString& strFieldName, const PString& strDbName = "main");
+private:
+    sqlite3 *     m_pDb;
+    sqlite3_stmt *m_pStmt;
 
-	public:
+    unsigned int *         m_pUseCount;
+    std::map<PString, int> m_mapFieldName2Index;
+};
 
-		// 开启事务，在需要多次执行SQL语句时，先开启事务可以提升很大效率
-		// SQLite缺省为每个操作启动一个事务
-		void beginTransaction();
+}  // namespace Sindy
 
-		// 回滚
-		void rollBack();
-
-		// 提交
-		void commit();
-
-		// 获取最近一次错误信息
-		PString errorMessage();
-
-	private:
-
-		sqlite3* m_pDb;
-		sqlite3_stmt* m_pStmt;
-
-		unsigned int* m_pUseCount;
-		std::map<PString, int> m_mapFieldName2Index;
-	};
-
-} // namespace Sindy
+#endif  // !SQLITE_UTILITY_U
