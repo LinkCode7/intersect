6245ea575c7360ece2374ea85e1889645b93f3af
LinkCode7 <siyujianglin@163.com>
2023-08-31 15:53:26 +0800

    modify

--- common/SindyRunTime.cpp
+++ common/SindyRunTime.cpp
@@ -2,14 +2,28 @@
 
 #include <chrono>
 #include <fstream>
+#include <iostream>
 #include <sstream>
+#include <thread>
 
 using namespace std::chrono;
 
 namespace sindy
 {
-RunTime::RunTime() : _firstTime(steady_clock::now()), _lastTime(_firstTime)
+RunTime::RunTime(std::string const& title)
 {
+    reset(title);
+}
+
+void RunTime::reset(std::string const& title)
+{
+    if (!title.empty())
+        std::cout << title << std::endl;
+
+    _mapLog2Time.clear();
+    _arrLog.clear();
+    _firstTime = steady_clock::now();
+    _lastTime  = _firstTime;
 }
 
 long RunTime::addTimePoint(std::string const& log)
@@ -18,43 +32,31 @@ long RunTime::addTimePoint(std::string const& log)
     auto      offset  = curTime - _lastTime;
     _lastTime         = curTime;
 
-    _addRunTime(log, offset);
-    long result = ConverToMilliseconds(offset).count();
-    return result;
-}
-
-void RunTime::reset()
-{
-    _mapLog2Time.clear();
-    _arrLog.clear();
-    _firstTime = steady_clock::now();
-    _lastTime  = _firstTime;
-}
-
-void RunTime::_addRunTime(std::string const& catalog, DurationTime offset)
-{
-    auto iter = _mapLog2Time.find(catalog);
+    auto iter = _mapLog2Time.find(log);
     if (iter != _mapLog2Time.end())
     {
         iter->second += offset;
     }
     else
     {
-        _mapLog2Time[catalog] = offset;
-        _arrLog.emplace_back(catalog);
+        _mapLog2Time[log] = offset;
+        _arrLog.emplace_back(log);
     }
+
+    long result = ConverToMilliseconds(offset).count();
+    return result;
 }
 
-std::string RunTime::str(std::string const& title) const
+std::string RunTime::str(std::string const& prefix) const
 {
     TimePoint curTime = steady_clock::now();
     long      all     = ConverToMilliseconds(curTime - _firstTime).count();
 
     std::stringstream ss;
-    if (title.empty())
-        ss << std::endl << "total: " << all << "ms" << std::endl;
+    if (prefix.empty())
+        ss << "total: " << all << "ms" << std::endl;
     else
-        ss << std::endl << title << " total: " << all << "ms" << std::endl;
+        ss << prefix << " total: " << all << "ms" << std::endl;
 
     for (const auto& log : _arrLog)
     {
@@ -62,24 +64,62 @@ std::string RunTime::str(std::string const& title) const
         if (iter == _mapLog2Time.end())
             continue;
 
-        ss << log << ": " << ConverToMilliseconds(iter->second).count() << "ms" << std::endl;
+        ss << "   " << log << ": " << ConverToMilliseconds(iter->second).count() << "ms" << std::endl;
     }
 
     return ss.str();
 }
 
-void RunTime::console(std::string const& title) const
+void RunTime::console(std::string const& prefix) const
 {
     std::cout << str();
 }
 
-void RunTime::write(std::string const& fullPath, std::string const& title) const
+void RunTime::write(std::string const& fullPath, std::string const& prefix) const
 {
     // std::locale&  loc = std::locale::global(std::locale(std::locale(), "", LC_CTYPE)); // 设全局locale为本地语言
     std::ofstream ofs(fullPath, std::ios::app);
     // std::locale::global(loc); // 恢复全局locale
 
-    ofs << str(title);
+    ofs << str(prefix);
+}
+
+void RunTime::testCase()
+{
+    // The first way
+    RunTime time("step");
+
+    std::cout << "step1..." << std::endl;
+    time.addTimePoint("step1");
+
+    std::cout << "step2..." << std::endl;
+    time.addTimePoint("step2");
+
+    time.console();
+
+    // The second way
+    auto f1 = [&]() {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        RunTime::instance().addTimePoint(".f1");
+    };
+
+    auto f2 = [&]() {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        RunTime::instance().addTimePoint("f2");
+        f1();
+    };
+
+    auto f = [&]() {
+        f1();
+        // f2();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    };
+
+    RunTime::instance().reset("call function");
+    f();
+    RunTime::instance().addTimePoint("f");
+
+    RunTime::instance().console("call function");
 }
 
 } // namespace sindy
--- common/SindyRunTime.h
+++ common/SindyRunTime.h
@@ -13,6 +13,8 @@
     #define ConverToMilliseconds(durationX) std::chrono::duration_cast<std::chrono::milliseconds>((durationX))
 #endif
 
+#define SINDY_RUN_TIME_SUB_CATLOG '.'
+
 namespace sindy
 {
 using TimePoint    = std::chrono::steady_clock::time_point;
@@ -25,18 +27,24 @@ class SINDY_API RunTime
     std::map<std::string, DurationTime> _mapLog2Time;
     std::vector<std::string>            _arrLog;
 
-    void _addRunTime(std::string const & catalog, DurationTime offset);
-
 public:
-    RunTime();
-    void reset();
+    static RunTime& instance()
+    {
+        static RunTime opr;
+        return opr;
+    }
+    RunTime(std::string const& title = "");
+    void reset(std::string const& title = "");
 
     // 获取上一个log到当前log流逝的时间
-    long addTimePoint(std::string const & log = "");
+    long addTimePoint(std::string const& log = "");
 
-    std::string str(std::string const & title = "") const;
-    void        console(std::string const & title = "") const;
-    void        write(std::string const & fullPath, std::string const & title = "") const;
+    std::string str(std::string const& prefix = "") const;
+    void        console(std::string const& prefix = "") const;
+    void        write(std::string const& fullPath, std::string const& prefix = "") const;
+
+    // method of application
+    static void testCase();
 };
 
 } // namespace sindy
